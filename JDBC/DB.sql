5b3927d1ee3865f9534a7c7538ae73f039c03477
Juan Daniel Castillo Reyes <castilloreyesjuan@gmail.com>
2015-10-16 21:33:24 -0500

    Conexion con POO

---
+++ JDBC/DB.sql
@@ -0,0 +1,22 @@
+create database platzi_java;
+use platzi_java;
+show tables;
+desc vehiculo;
+create table vehiculo(
+	vehiculo_id int primary key,
+    placa nvarchar(10) not null default 'XXX',
+    nombre nvarchar(15) not null,
+    marca nvarchar(15) not null
+);
+insert into vehiculo values
+(1,'PLACA2','IBIZA','SEAT'),
+(2,'PLACA3','GOL','NISSAN'),
+(3,'PLACA4','SUNFIRE','PONTIAC'),
+(4,'PLACA5','CRV','NISSAN'),
+(5,'PLACA6','A3','AUDI');
+select * from vehiculo;
+drop table vehiculo;
+#Ver Conexiones Activas
+	show processlist;
+#Ver Conecciones y datos de la session
+	show session status;
