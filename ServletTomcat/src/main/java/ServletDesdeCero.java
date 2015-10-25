c88b840f94303a4daadbe813e7dc466fff081b0e
Juan Daniel Castillo Reyes <castilloreyesjuan@gmail.com>
2015-10-25 16:45:24 -0600

    Servlet Practica Running on Tomcat

---
+++ ServletTomcat/src/main/java/ServletDesdeCero.java
@@ -0,0 +1,10 @@
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+//@WebServlet es una "anotation", esto es metadata que le dice al servidor que esto es un servlet
+@WebServlet("/ServletCero")
+public class ServletDesdeCero extends HttpServlet{
+    
+}
