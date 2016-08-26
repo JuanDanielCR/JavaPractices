22116fe5ea9c579793be93cf1adfd94603b3f825
JuanDanielCR <castilloreyesjuan@gmail.com>
2016-08-25 19:42:40 -0500

    HAciedo git reset

--- ESCOM/combinabits.java
+++ ESCOM/combinabits.java
@@ -1,15 +1,18 @@
-import java.util.ArrayList;
-public class Saludo{
+public class combinabits{
 
 	public static void main(String[] args) {
-		 int value = 5;
+		 int value = 40;
+		 StringBuilder binary = new StringBuilder("");
 		 for (int i = 0; i < Math.pow(2, value); i++) {
-		 StringBuilder binary = new StringBuilder(Integer.toBinaryString(i));
-		 System.out.println("Antes: "+binary+ "binary - length: "+binary.length());
-		 for(int j = binary.length(); j < value; j++) {
-				 binary.insert( 0, '0' );
-		 }
-		 System.out.println("Despues:"+binary);
- 	}
-}
+			 binary.insert(0, Integer.toBinaryString(i));
+			 for(int j = binary.length(); j < value; j++) {
+					 binary.insert( 0, '0' );
+			 }
+			 System.out.print("{"+binary+"},");
+			 int largo_combinacion = binary.length();
+			 for(int k = 0; k < largo_combinacion; k++) {
+				 binary.deleteCharAt(0);
+			 }
+ 		}
+	}
 }
