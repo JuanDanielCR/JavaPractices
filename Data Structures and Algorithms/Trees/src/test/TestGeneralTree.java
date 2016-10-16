ebd0e8e4d20c0f5eb54862d1fe01880b26866f88
JuanDanielCR <castilloreyesjuan@gmail.com>
2016-10-16 13:02:21 -0500

    Divide y venceras

--- Data Structures and Algorithms/Trees/src/test/TestGeneralTree.java
+++ Data Structures and Algorithms/Trees/src/test/TestGeneralTree.java
@@ -1,5 +1,9 @@
 package test;
 
+import sources.*;
 public class TestGeneralTree {
-	
+	public static void main(String args[]){
+		GeneralTree<Integer> tree = new GeneralTree<>();
+		sources.GeneralTree.Node<Integer> root =new sources.GeneralTree.Node<Integer>(0);
+	}
 }
