136a4b0b963a80995f93e2e2a041c52223bd8ed2
JuanDanielCR <castilloreyesjuan@gmail.com>
2016-10-16 20:41:39 -0500

    Non-deterministic using general tree as ADT

---
+++ ESCOM/TeoriaComputacional/Chess/src/test/Launch.java
@@ -0,0 +1,9 @@
+package test;
+import sources.*;
+public class Launch {
+	public static void main(String args[]){
+		Scanning sc = new Scanning();
+		sc.getCharacter();
+		System.out.println("He terminado");
+	}
+}
