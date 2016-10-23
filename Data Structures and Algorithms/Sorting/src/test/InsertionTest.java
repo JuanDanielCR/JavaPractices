a80bb85bfc093b36ec66e73550a9c0d471bb078f
JuanDanielCR <castilloreyesjuan@gmail.com>
2016-10-22 22:11:15 -0500

    Lists Carpet

--- Data Structures and Algorithms/Sorting/src/test/InsertionTest.java
+++ Data Structures and Algorithms/Sorting/src/test/InsertionTest.java
@@ -1,17 +1,32 @@
 package test;
+import java.util.Random;
+
 import sources.InsertionSorting;
 public class InsertionTest {
 	public static void main(String args[]){
 		int data[] =  new int[1000];
+		
+		Random rand = new Random( ); // a pseudo-random number generator
+		rand.setSeed(System.currentTimeMillis( ));
+		
 		for(int i=0; i<1000; i++){
-			int aux = (int)(Math.random()*10);
+			int aux = rand.nextInt(100);
 			data[i] = aux;
 			System.out.print(aux);
 		}
+		
 		System.out.println(" ");
+		
 		InsertionSorting.insertionSort(data);
 		for(int i=0; i<1000; i++){
 			System.out.print(data[i]); 
+		}
+		
+		String h = "hola";
+		char c[] = h.toCharArray();
+		
+		for(int i=0; i<c.length; i++){
+			System.out.print(c[i]); 
 		}
 	}
 }
