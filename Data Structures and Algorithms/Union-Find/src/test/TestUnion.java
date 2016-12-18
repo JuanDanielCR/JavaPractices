8200a7958a8d71ff9277176892cf6a63ba5e7f08
JuanDanielCR <castilloreyesjuan@gmail.com>
2016-12-18 14:23:42 -0600

    Quick Weighted Union with Path Compression

--- Data Structures and Algorithms/Union-Find/src/test/TestUnion.java
+++ Data Structures and Algorithms/Union-Find/src/test/TestUnion.java
@@ -10,7 +10,32 @@ public class TestUnion {
 	public static void main(String args[]){
 		Random rdn = new Random();
 		
-
+		//Quick Find -> N = 1x10E6 (more than 1 minute)
+		QuickFind qf =  new QuickFind(N);
+		System.out.println("Quick find");
+		for(int i = 0; i < N; i++)
+			qf.union(rdn.nextInt(100),rdn.nextInt(100));
+		for(int i = 0; i < N; i++)
+			System.out.println(qf.isConnected(rdn.nextInt(100),rdn.nextInt(100)));
+		qf =  null;
+		
+		//Quick Union -> N = 1x10E7 (more than 1 minute)
+		QuickUnion qu =  new QuickUnion(N);
+		System.out.println("Quick Union");
+		for(int i = 0; i < N; i++)
+			qu.union(rdn.nextInt(100),rdn.nextInt(100));
+		for(int i = 0; i < N; i++)
+			System.out.println(qu.isConnected(rdn.nextInt(100),rdn.nextInt(100)));
+		qu =  null;
+		
+		//Quick Union Weighted -> N = 1x10E7 (more than 1 minute)
+		QuickUnionWeighted quw = new QuickUnionWeighted(N);
+		System.out.println("Quick Union Weighted");
+		for(int i = 0; i < N; i++)
+			quw.union(rdn.nextInt(100),rdn.nextInt(100));
+		for(int i = 0; i < N; i++)
+			System.out.println(quw.isConnected(rdn.nextInt(100),rdn.nextInt(100)));
+		quw =  null;
 		
 		//Quick Union Weighted Path Compression -> N = 1x10E8 (more than 1 minute)
 		QuickUnionWeighted quwp = new QuickUnionWeighted(N);
@@ -18,7 +43,7 @@ public class TestUnion {
 		for(int i = 0; i < N; i++)
 			quwp.union(rdn.nextInt(100),rdn.nextInt(100));
 		for(int i = 0; i < N; i++)
-			System.out.println(".");
+			System.out.println(quwp.isConnected(rdn.nextInt(100),rdn.nextInt(100)));
 		quwp = null;
 		
 	}
