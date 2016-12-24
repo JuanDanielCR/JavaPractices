8772d64c478ac2744dedd711bdc8853799fcef8b
JuanDanielCR <castilloreyesjuan@gmail.com>
2016-12-24 16:45:25 -0600

    SendGrid Integration

--- SendGrid/Sendgrid/src/main/java/lifesoft/Sendgrid/App.java
+++ SendGrid/Sendgrid/src/main/java/lifesoft/Sendgrid/App.java
@@ -9,11 +9,11 @@ public class App
 	  public static void main(String[] args) throws IOException {
 	    Email from = new Email("test@example.com");
 	    String subject = "Sending with SendGrid is Fun";
-	    Email to = new Email("armandocreyes02@gmail.com");
-	    Content content = new Content("text/html", "<h1>Titulote</h1>");
+	    Email to = new Email("castilloreyesjuan@gmail.com");
+	    Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
 	    Mail mail = new Mail(from, subject, to, content);
 
-	    SendGrid sg = new SendGrid("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");
+	    SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
 	    Request request = new Request();
 	    try {
 	      request.method = Method.POST;
