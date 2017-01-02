d5f5efc128fdde2661e05a8d838a490ac62e5296
JuanDanielCR <castilloreyesjuan@gmail.com>
2017-01-01 18:55:56 -0600

    Deleting repeating data

--- Chessways/src/test/Launch.java
+++
@@ -1,9 +0,0 @@
-package test;
-import sources.*;
-public class Launch {
-	public static void main(String args[]){
-		Scanning sc = new Scanning();
-		sc.getCharacter();//Manual mode
-		System.out.println("He terminado");
-	}
-}
--- webay/src/test/Launch.java
+++
@@ -1,8 +0,0 @@
-package test;
-import sources.*;
-public class Launch {
-	public static void main(String args[]){
-		Archivo a = new Archivo("ebay.txt", "salida.txt", "historia.txt");
-		a.analizarArchivo();
-	}
-}
